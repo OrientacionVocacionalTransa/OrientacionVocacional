2701cafea1773a04c69cebe93165a245d7d36a67
ALVI3ZZZ <150144552+ALVI3ZZZ@users.noreply.github.com>
2024-09-18 23:10:37 +0000

    pago

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { PagoComponent } from './pago/pago.component'; // Importa el componente de pago
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PrincipalComponent }, // Página principal
+  { path: 'pago', component: PagoComponent }, // Ruta de pago
+  { path: '', redirectTo: '' } // Redirección por defecto
+];
