a98447afe9c4d47aee6bbded7ad446032c8c7b1f
ALVI3ZZZ <150144552+ALVI3ZZZ@users.noreply.github.com>
2024-09-19 03:49:32 +0000

    PlanesFinal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PagoComponent } from './pago/pago.component'; // Importa el componente de pago
+import { PlanesComponent } from './planes/planes.component';
 
 export const routes: Routes = [
   { path: '', component: PagoComponent }, // Página principal ahora es "Pago"
+  {path: 'planes', component: PlanesComponent},
   { path: '**', redirectTo: '' } // Redirección por defecto a "Pago" en caso de rutas no encontradas
 ];
