912e5df6f8c0e3d3b4b55ef3df1a0f2462329688
PieroSanchez <pierosancas@gmail.com>
2024-09-18 23:48:25 -0500

    design-interface-login

---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { first, firstValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+
+
+export class LoginComponent {
+  
+  constructor(
+    private fb: FormBuilder, 
+    private http: HttpClient
+  ){
+  
+  }
+
+}
+
