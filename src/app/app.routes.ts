00a46b28833f57ea47a96dfd1ef0f252fbeb62c1
Yuliana Cacho <ycachoq1@upao.edu.pe>
2024-11-13 00:29:21 -0500

    AsesoriaEstudiante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { PreguntaComponent } from './pregunta/pregunta.component';
+import { AsesoriaEstudianteComponent } from './asesoria-estudiante/asesoria-estudiante.component';
 
 export const routes: Routes = [
   { path: '', component: PrincipalComponent }, // Página principal
   {path:'pregunta', component: PreguntaComponent},
+  {path:'asesoria', component: AsesoriaEstudianteComponent},
   { path: '**', redirectTo: '' } // Redirección por defecto
 ];
---
+++ src/app/asesoria-estudiante/asesoria-estudiante.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asesoria-estudiante',
+  standalone: true,
+  imports: [],
+  templateUrl: './asesoria-estudiante.component.html',
+  styleUrl: './asesoria-estudiante.component.scss'
+})
+export class AsesoriaEstudianteComponent {
+
+}
